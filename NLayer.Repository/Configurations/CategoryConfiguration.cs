eb3bc9798b6332b732907cc4fad25a770edb6aba
Onur Kırık <kirikonurr@gmail.com>
2023-03-20 23:41:09 +0300

    Add project files.

---
+++ NLayer.Repository/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).UseIdentityColumn();
+            builder.Property(c => c.Name).IsRequired().HasMaxLength(50);
+
+            builder.ToTable("Categories");
+        }
+    }
+}
---
+++ NLayer.Repository/Configurations/ProductFeatureConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    public class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).UseIdentityColumn();
+
+            builder.HasOne(pf => pf.Product).WithOne(p => p.ProductFeature).HasForeignKey<ProductFeature>(pf => pf.ProductId);
+        }
+    }
+}
